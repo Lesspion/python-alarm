12351c3e00ce04039b0f2cc16d89c0082ab49315
lesspion <coquelet.c@gmail.com>
2015-04-11 16:41:59 +0200

    test win/unix

--- alarm.py
+++ alarm.py
@@ -7,17 +7,26 @@ import os
 import time
 import wave
 import sys
+import platform
 
+print(os.getlogin())
+print(sys.platform)
 alarm = "heure-min";
-curHour = int(time.strftime("%H", time.gmtime())) + 1
+curHour = int(time.strftime("%H", time.gmtime())) + 2
 curMinutes = time.strftime("%M", time.gmtime())
 curSeconds = time.strftime("%S", time.gmtime())
 print(str(curHour) + ":" + str(curMinutes) + ":" + str(curSeconds))
-while(str(curHour) + ":" + str(curMinutes) + ":" + str(curSeconds) != "23:48:10"):
+while(str(curHour) + ":" + str(curMinutes) + ":" + str(curSeconds) != "5:30:00"):
     time.sleep(1)
-    curHour = int(time.strftime("%H", time.gmtime())) + 1
+    curHour = int(time.strftime("%H", time.gmtime())) + 2
     curMinutes = time.strftime("%M", time.gmtime())
     curSeconds = time.strftime("%S", time.gmtime())
     print(str(curHour) + ":" + str(curMinutes) + ":" + str(curSeconds))
 
-os.startfile('Rengar le Chat - Remix de la nouvelle daube de Corobizar.mp3')
+os.system('vlc ~/Musique/lightbringer.mp3')
+#os.startfile('Rengar le Chat - Remix de la nouvelle daube de Corobizar.mp3')
+
+if (sys.platform == "linux"):
+	os.system('vlc ~/Musique/lightbringer.mp3')
+else:
+	os.startfile('~/Musique/lightbringer.mp3')
